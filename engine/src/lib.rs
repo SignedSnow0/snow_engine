4a3bf2bdda68320da5ab9102862edde113587d38
SignedSnow0 <claudiomarchini2003@gmail.com>
2023-03-06 14:18:56 +0100

    Added shader compilation

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,15 +1,4 @@
 pub mod platform;
 
 #[cfg(test)]
-mod tests {
-    use crate::platform::vulkan::vk_core::VkCore;
-    use crate::platform::vulkan::vk_surface::VkSurface;
-    use crate::platform::window::Window;
-
-    #[test]
-    fn create_vk_core() {
-        let core = VkCore::new().unwrap();
-
-        core.query_physical_devices();
-    }
-}
+mod tests {}
